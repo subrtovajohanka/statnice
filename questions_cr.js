580e6f8f66f33745d7cbc89332619f59f1436d88
Johanka Šubrtová <subrtovajohanka@gmail.com>
2025-05-05 10:38:13 +0200

    Add files via upload

---
+++ questions_cr.js
@@ -0,0 +1,12 @@
+const questions = [
+  {
+    question: "Jaká je hlavní funkce destinačního managementu?",
+    options: [
+      "Těžba surovin v regionu",
+      "Plánování a koordinace rozvoje cestovního ruchu",
+      "Kontrola kvality potravin",
+      "Organizace letecké dopravy"
+    ],
+    answer: 1
+  }
+];
